d669c264a11de492ac539ad8ba0f2c3a782a91cb
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-16 19:59:17 +0800

    Add files via upload

--- spring/src/main/java/com/study/spring/case03/model/User.java
+++ spring/src/main/java/com/study/spring/case03/model/User.java
@@ -6,27 +6,34 @@ import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:user.properties")
 public class User {
 	
-	@Value(value = "John")
+	//@Value(value = "John")
+	@Value("${user.name1}")
 	private String name;
 	
-	@Value(value = "18")
+	//@Value(value = "18")
+	@Value("${user.age}")
 	private Integer age;
 	
-	@Value("#{${nikename : {'Happy', 'Enjoy'}}}")  // Spring EL
+	//@Value("#{${nikename : {'Happy', 'Enjoy'}}}")  // Spring EL
+	@Value("${user.nikename}") 
 	private String[] nikename; // 暱稱
 	
 	@Value("#{${subjects : {'國文', '英文', '數學'}}}")
 	private Set<String> subjects; // 科目
 	
-	@Value("#{${scores : {100, 90, 100}}}")
+	//@Value("#{${scores : {100, 90, 100}}}")
+	@Value("#{'${user.scores}'.split(',')}")
 	private List<Integer> scores; // 成績
 	
-	@Value("#{${hobbies : {key1: 'Car', key2: 'Airplane'}}}")
+	//@Value("#{${hobbies : {key1: 'Car', key2: 'Airplane'}}}")
+	@Value("#{${user.hobbies}}")
 	private Map<String, String> hobbies;
 
 	public String getName() {
