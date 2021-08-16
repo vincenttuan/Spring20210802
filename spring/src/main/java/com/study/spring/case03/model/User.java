ee536186439e13246069086fa386e365785485ba
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-16 19:37:13 +0800

    Add files via upload

--- spring/src/main/java/com/study/spring/case03/model/User.java
+++ spring/src/main/java/com/study/spring/case03/model/User.java
@@ -1,5 +1,10 @@
 package com.study.spring.case03.model;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +16,18 @@ public class User {
 	
 	@Value(value = "18")
 	private Integer age;
+	
+	@Value("#{${nikename : {'Happy', 'Enjoy'}}}")  // Spring EL
+	private String[] nikename; // 暱稱
+	
+	@Value("#{${subjects : {'國文', '英文', '數學'}}}")
+	private Set<String> subjects; // 科目
+	
+	@Value("#{${scores : {100, 90, 100}}}")
+	private List<Integer> scores; // 成績
+	
+	@Value("#{${hobbies : {key1: 'Car', key2: 'Airplane'}}}")
+	private Map<String, String> hobbies;
 
 	public String getName() {
 		return name;
@@ -28,10 +45,44 @@ public class User {
 		this.age = age;
 	}
 
+	public String[] getNikename() {
+		return nikename;
+	}
+
+	public void setNikename(String[] nikename) {
+		this.nikename = nikename;
+	}
+
+	public Set<String> getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(Set<String> subjects) {
+		this.subjects = subjects;
+	}
+
+	public List<Integer> getScores() {
+		return scores;
+	}
+
+	public void setScores(List<Integer> scores) {
+		this.scores = scores;
+	}
+
+	public Map<String, String> getHobbies() {
+		return hobbies;
+	}
+
+	public void setHobbies(Map<String, String> hobbies) {
+		this.hobbies = hobbies;
+	}
+
 	@Override
 	public String toString() {
-		return "User [name=" + name + ", age=" + age + "]";
+		return "User [name=" + name + ", age=" + age + ", nikename=" + Arrays.toString(nikename) + ", subjects="
+				+ subjects + ", scores=" + scores + ", hobbies=" + hobbies + "]";
 	}
+	
 	
 	
 }
