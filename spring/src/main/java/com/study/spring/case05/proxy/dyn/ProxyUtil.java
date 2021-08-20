495c7f97ebbd778f6cb181a0810f1d04b858551d
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-20 20:06:14 +0800

    Add files via upload

---
+++ spring/src/main/java/com/study/spring/case05/proxy/dyn/ProxyUtil.java
@@ -0,0 +1,28 @@
+package com.study.spring.case05.proxy.dyn;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class ProxyUtil {
+	private Object object;
+
+	public ProxyUtil(Object object) {
+		this.object = object;
+	}
+	
+	public Object getProxy() {
+		// 類別載入器
+		ClassLoader loader = getClass().getClassLoader();
+		// 目標所實作的介面
+		Class[] interfaces = object.getClass().getInterfaces();
+		// 處理代理的實現
+		InvocationHandler handler = (Object proxy, Method method, Object[] args) -> {
+			return null;
+		}; 
+		
+		Object proxyObj = Proxy.newProxyInstance(loader, interfaces, handler);
+		return proxyObj;
+	}
+	
+}
