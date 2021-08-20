f3b1fdacbe1f720c95c916f6faa024eb3013eb7b
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-20 20:27:46 +0800

    Add files via upload

--- spring/src/main/java/com/study/spring/case05/proxy/dyn/ProxyUtil.java
+++ spring/src/main/java/com/study/spring/case05/proxy/dyn/ProxyUtil.java
@@ -3,6 +3,7 @@ package com.study.spring.case05.proxy.dyn;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
 
 public class ProxyUtil {
 	private Object object;
@@ -13,12 +14,32 @@ public class ProxyUtil {
 	
 	public Object getProxy() {
 		// 類別載入器
-		ClassLoader loader = getClass().getClassLoader();
+		ClassLoader loader = object.getClass().getClassLoader();
 		// 目標所實作的介面
 		Class[] interfaces = object.getClass().getInterfaces();
 		// 處理代理的實現
 		InvocationHandler handler = (Object proxy, Method method, Object[] args) -> {
-			return null;
+			Object result = null;
+			try {
+				// 前置通知
+				System.out.println("前置 Log:" + 
+								   object.getClass() + "," + 
+								   method.getName() + "," + 
+								   Arrays.toString(args));
+				result = method.invoke(object, args); // 代理呼叫方法
+				return result;
+			} catch (Exception e) {
+				//e.printStackTrace(System.out);
+				// 例外異常通知
+				System.out.println("例外 Log:" + object.getClass() + "," + e);
+			} finally {
+				// 後置通知
+				System.out.println("後置 Log:" + 
+						   object.getClass() + "," + 
+						   method.getName() + "," + 
+						   result);
+			}
+			return result;
 		}; 
 		
 		Object proxyObj = Proxy.newProxyInstance(loader, interfaces, handler);
