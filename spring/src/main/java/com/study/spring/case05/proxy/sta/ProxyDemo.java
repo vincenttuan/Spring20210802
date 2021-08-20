2ddf33c7f3d26de759b9c16798284419f95614d3
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-20 19:39:18 +0800

    Add files via upload

---
+++ spring/src/main/java/com/study/spring/case05/proxy/sta/ProxyDemo.java
@@ -0,0 +1,61 @@
+package com.study.spring.case05.proxy.sta;
+
+interface Person {
+	void work();
+}
+
+class Man implements Person {
+	@Override
+	public void work() {
+		System.out.println("上班工作");
+	}
+}
+
+class Woman implements Person {
+	@Override
+	public void work() {
+		System.out.println("去市場買菜");
+	}
+}
+
+// 靜態代理
+class PersonProxy implements Person {
+	private Person person;
+	
+	public PersonProxy(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public void work() {
+		// 前-公用方法
+		System.out.println("戴口罩");
+		try {
+			// 代理調用-業務方法
+			person.work();
+		} catch (Exception e) {
+			// 例外公用方法
+			System.out.println("買口罩");
+		}
+		// 後-公用方法
+		System.out.println("脱口罩");
+	}
+	
+} 
+
+
+public class ProxyDemo {
+	public static void main(String[] args) {
+		// 不透過代理, 直接執行
+		/*
+		Person p1 = new Man();
+		Person p2 = new Woman();
+		p1.work();
+		p2.work();
+		*/
+		Person p1 = new PersonProxy(new Man());
+		Person p2 = new PersonProxy(new Woman());
+		p1.work();
+		p2.work();
+	}
+}
