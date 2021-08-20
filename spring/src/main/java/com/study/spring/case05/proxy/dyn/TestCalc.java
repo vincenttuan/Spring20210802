e3860867d0f48f838c6c9b1d75d03fb47aee7f29
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-20 21:07:37 +0800

    Add files via upload

--- spring/src/main/java/com/study/spring/case05/proxy/dyn/TestCalc.java
+++ spring/src/main/java/com/study/spring/case05/proxy/dyn/TestCalc.java
@@ -3,6 +3,7 @@ package com.study.spring.case05.proxy.dyn;
 public class TestCalc {
 
 	public static void main(String[] args) {
+		
 		Calc calc = (Calc)new ProxyUtil(new CalcImpl()).getProxy();
 		System.out.println(calc.add(10, 20));
 		
---
+++ spring/src/main/java/com/study/spring/case05/proxy/dyn/TestPerson.java
@@ -0,0 +1,10 @@
+package com.study.spring.case05.proxy.dyn;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// 實現 Man 與 Woman 的工作
+		
+	}
+
+}
