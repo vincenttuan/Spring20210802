1b70cbb972b0fb79ac9e43ea7e5ef3b63d5257ea
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-27 19:26:35 +0800

    Add files via upload

---
+++ spring/src/main/java/com/study/spring/case05/aop_dancer/Introducter.java
@@ -0,0 +1,17 @@
+package com.study.spring.case05.aop_dancer;
+
+import org.aspectj.lang.annotation.DeclareParents;
+
+public class Introducter { // 經紀人
+	
+	// 將 Performance 及其子類別轉換為 Singer
+	@DeclareParents(value = "com.study.spring.case05.aop_dancer.Performance+", 
+					defaultImpl = BackSinger.class)
+	public Singer singer;
+	
+	// 將 Performance 及其子類別轉換為 Actor
+	@DeclareParents(value = "com.study.spring.case05.aop_dancer.Performance+", 
+			defaultImpl = TalkActor.class)
+	public Actor actor;
+
+}
