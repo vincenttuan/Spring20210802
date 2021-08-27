f0f0d654b126c0cb15ecfa6ae756dce84c62ffc0
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-27 21:59:54 +0800

    Add files via upload

--- spring/src/main/java/com/study/spring/case06/jdbc/SelectEmp.java
+++ spring/src/main/java/com/study/spring/case06/jdbc/SelectEmp.java
@@ -14,12 +14,19 @@ public class SelectEmp {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
 		JdbcTemplate jdbcTemplate = ctx.getBean("jdbcTemplate", JdbcTemplate.class);
 		
+		// 多筆查詢
 		String sql = "select eid, ename, age, createtime from emp";
 		List<Map<String, Object>> emps = jdbcTemplate.queryForList(sql);
 		for(Map<String, Object> emp : emps) {
 			//System.out.println(emp);
 			System.out.println(emp.get("ename"));
 		}
+		
+		// 單筆單欄查詢
+		// 現在 emp 資料表中有幾筆資料 ?
+		sql = "select count(*) from emp";
+		Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
+		System.out.println("資料筆數: " + count);
 	}
 
 }
