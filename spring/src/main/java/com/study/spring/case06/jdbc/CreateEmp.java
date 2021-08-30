c4323c03bc5d40799128526b09a1ce91830b33af
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-30 19:52:36 +0800

    Add files via upload

--- spring/src/main/java/com/study/spring/case06/jdbc/CreateEmp.java
+++ spring/src/main/java/com/study/spring/case06/jdbc/CreateEmp.java
@@ -1,5 +1,9 @@
 package com.study.spring.case06.jdbc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,19 +16,29 @@ public class CreateEmp {
 	
 	@Test
 	public void test() {
-		case1();
+		//case1();
+		case2();
 	}
 	
+	// 單筆新增
 	public void case1() {
 		String sql = "Insert into emp(ename, age) values(?, ?)";
 		jdbcTemplate.update(sql, "Jo", 21);
 		System.out.println("Insert ok 1");
 	}
 	
+	// 多筆新增 I
 	public void case2() {
-		
+		String sql = "Insert into emp(ename, age) values(?, ?)";
+		List<Object[]> list = new ArrayList<>();
+		list.add(new Object[] {"Jean", 22});
+		list.add(new Object[] {"helen", 22});
+		list.add(new Object[] {"jack", 24});
+		int[] rows = jdbcTemplate.batchUpdate(sql, list);
+		System.out.println(Arrays.toString(rows));
 	}
 	
+	// 多筆新增 II
 	public void case3() {
 		
 	}
