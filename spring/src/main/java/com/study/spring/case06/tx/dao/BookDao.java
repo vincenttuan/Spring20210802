317a5c0b2e0067c112f6507668a88f9c0fccdb91
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-30 21:38:17 +0800

    Add files via upload

---
+++ spring/src/main/java/com/study/spring/case06/tx/dao/BookDao.java
@@ -0,0 +1,9 @@
+package com.study.spring.case06.tx.dao;
+
+public interface BookDao {
+	
+	Integer getPrice(Integer bid);
+	Integer updateStock(Integer bid);
+	Integer updateWallet(Integer wid, Integer money); // 扣掉金額
+
+}
