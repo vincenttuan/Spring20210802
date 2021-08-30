79c1322cad5c9d0af101f65085bb3b3869779d09
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-08-30 19:30:19 +0800

    Add files via upload

--- spring/src/main/java/com/study/spring/case06/jdbc/SelectEmp.java
+++ spring/src/main/java/com/study/spring/case06/jdbc/SelectEmp.java
@@ -1,11 +1,14 @@
 package com.study.spring.case06.jdbc;
 
+import java.sql.ResultSet;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class SelectEmp {
 
@@ -27,6 +30,22 @@ public class SelectEmp {
 		sql = "select count(*) from emp";
 		Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
 		System.out.println("資料筆數: " + count);
+		
+		// 多筆查詢-Entity
+		sql = "select eid, ename, age, createtime from emp";
+		List<Emp> listEmps = jdbcTemplate.query(sql, (ResultSet rs, int rowNum)->{
+			Emp emp = new Emp();
+			Integer eid = rs.getInt("eid");
+			String ename = rs.getString("ename");
+			Integer age = rs.getInt("age");
+			Date createtime = rs.getDate("createtime");
+			emp.setEid(eid);
+			emp.setEname(ename);
+			emp.setAge(age);
+			emp.setCreatetime(createtime);
+			return emp;
+		});
+		System.out.println(listEmps);
 	}
 
 }
