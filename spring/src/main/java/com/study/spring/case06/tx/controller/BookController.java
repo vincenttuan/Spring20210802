cc1605bfa78fc8fa16c42e31243f9d06e6135c77
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-09-03 21:57:16 +0800

    Delete spring/src/main/java/com/study/spring/case06/tx directory

--- spring/src/main/java/com/study/spring/case06/tx/controller/BookController.java
+++
@@ -1,6 +0,0 @@
-package com.study.spring.case06.tx.controller;
-
-public interface BookController {
-	void buyBook(Integer wid, Integer bid);
-	void buyBooks(Integer wid, Integer... bids);
-}
--- spring/src/main/java/com/study/spring/case06/tx/service/BookService.java
+++
@@ -1,6 +0,0 @@
-package com.study.spring.case06.tx.service;
-
-public interface BookService {
-	void buyOne(Integer wid, Integer bid);
-	void buyMany(Integer wid, Integer... bid);
-}
